27bdbb6c50cebed901cc5730f245a599f9083497
miguel-aviles <elmondayuca@gmail.com>
2023-04-09 20:53:57 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { SelectList } from 'react-native-dropdown-select-list'
+import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selected, setSelected] = React.useState("");
+  
+  const data = [
+      {key:'1', value:'4x4'},
+      {key:'2', value:'5x5'},
+      {key:'3', value:'6x6'},
+      {key:'4', value:'7x7'},
+      {key:'5', value:'8x8'},
+      {key:'6', value:'9x9'},
+      {key:'7', value:'10x10'},
+  ]
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Choose your game's dimensions</Text>
+            <SelectList 
+                setSelected={(val) => setSelected(val)} 
+                data={data} 
+                save="value"
+            />
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Done</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle}>Play game</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  centeredView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: 'white',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'rg',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: 'rgb(206, 203, 255)',
+  },
+  buttonClose: {
+    marginTop: 25,
+    backgroundColor: 'rgb(206, 203, 255)',
+  },
+  textStyle: {
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
+
+export default App;
